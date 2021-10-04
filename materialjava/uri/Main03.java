07b518297f1835bfa33748552c426e9f28ed4deb
syaoranea <eduardo_rocha87@hotmail.com>
2021-10-04 09:26:42 -0300

    aula 5 - exercicios

--- materialjava/uri/Main03.java
+++ materialjava/uri/Main03.java
@@ -1,19 +1,28 @@
 import java.util.Scanner;
 
 public class Main03 {
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        double a, b, c, media;
-
-        a = entrada.nextDouble();
-        b = entrada.nextDouble();
-        c = entrada.nextDouble();
-        
-        media = ((a * 0.2) + (b * 0.3) + (c * 0.5)) / 3 ;
-
-        System.out.printf("MEDIA = %.1f\n", media);
-        
-
-        entrada.close();
-    }
+    
+        public static void main(String[] args) {
+            double a, b, c, media;
+    
+            Scanner entrada = new Scanner(System.in);
+    
+            do {
+                a = entrada.nextDouble();
+            } while (a < 0 || a > 10);
+    
+            do {
+                b = entrada.nextDouble();
+            } while (b < 0 || b > 10);
+    
+            do {
+                c = entrada.nextDouble();
+            } while (c < 0 || c > 10);
+    
+            media = a* 0.2 + b * 0.3 + c * 0.5;
+    
+            System.out.printf("MEDIA = %.1f\n", media);
+    
+            entrada.close();
+        }
 }
